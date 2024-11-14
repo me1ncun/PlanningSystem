b4f8eeb7a1983441393dc22e39f261aa80153048
Denis <denis09112004@gmail.com>
2024-11-14 14:48:46 +0200

    Added Solution

---
+++ Software-Development-Technologies/Controllers/Controller.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Software_Development_Technologies.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Controller : ControllerBase
+{
+    
+}
