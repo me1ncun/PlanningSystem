24a666a4306184c29b1cc849729280fe3bc2a208
Denis <denis09112004@gmail.com>
2024-11-20 16:44:04 +0200

    Added Database and registered in DI

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnection"));
+});
 
 var app = builder.Build();
 
---
+++ DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Entities;
+using DAL.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Request> Requests { get; set; }
+    public DbSet<MedicalOpinion> MedicalOpinions { get; set; }
+}
