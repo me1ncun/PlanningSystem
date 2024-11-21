7ab445f9ec08aeafd74c0dbe006def404d3b580c
Denis <denis09112004@gmail.com>
2024-11-21 14:40:53 +0200

    Created AutoMapper Profile

---
+++ BLL/Profile/MapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using BLL.DTOs;
+using DAL.Entities;
+
+namespace BLL.Profile;
+
+using AutoMapper;
+
+public class MapperProfile : Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<Employee, EmployeeDto>().ReverseMap();
+        CreateMap<Request, RequestDto>().ReverseMap();
+        CreateMap<Request, RequestShortDto>().ReverseMap();
+        CreateMap<MedicalOpinion, MedicalOpinionDto>().ReverseMap();
+    }
+}
