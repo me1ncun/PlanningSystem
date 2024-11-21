331f67dc5e0b4c4e07dc0f5eceae324e42559e1f
Denis <denis09112004@gmail.com>
2024-11-21 14:41:02 +0200

    Registered services in di container

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,11 @@
+using BLL.Profile;
+using BLL.Services.Impl;
+using BLL.Services.Interfaces;
 using DAL.Data;
+using DAL.Entities;
+using DAL.Repositories.Impl;
+using DAL.Repositories.Interfaces;
+using DAL.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +22,24 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnection"));
 });
+
+// Register services
+builder.Services.AddScoped<IRequestService, RequestService>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IMedicalOpinionService, MedicalOpinionService>();
+
+builder.Services.AddScoped<IRequestRepository, RequestRepository>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IMedicalOpinionRepository, MedicalOpinionRepository>();
+
+// Add AutoMapper
+builder.Services.AddAutoMapper(typeof(MapperProfile));
+
+// Register UniOfWork
+builder.Services.AddScoped<IUnitOfWork, EFUnitOfWork>();
+
+// Register IRepository
+builder.Services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
 
 var app = builder.Build();
 
