7d1ece1ad84f2356e3282c3151b214b41c6c637b
Denis <denis09112004@gmail.com>
2024-11-21 14:36:13 +0200

    Created database migrations

--- DAL/Data/ApplicationDbContext.cs
+++ DAL/Data/ApplicationDbContext.cs
@@ -9,7 +9,7 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
-    
+
     public DbSet<Employee> Employees { get; set; }
     public DbSet<Request> Requests { get; set; }
     public DbSet<MedicalOpinion> MedicalOpinions { get; set; }
---
+++ DAL/Migrations/20241121101302_Initial.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    surname = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: true),
+                    phone = table.Column<string>(type: "text", nullable: false),
+                    role = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "medical_opinions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    conclusion = table.Column<string>(type: "text", nullable: true),
+                    is_healthy = table.Column<bool>(type: "boolean", nullable: false),
+                    EmployeeId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_medical_opinions", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_medical_opinions_users_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "requests",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    type = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    start_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    end_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    comment = table.Column<string>(type: "text", nullable: false),
+                    EmployeeId = table.Column<int>(type: "integer", nullable: false),
+                    OpinionId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_requests", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_requests_medical_opinions_OpinionId",
+                        column: x => x.OpinionId,
+                        principalTable: "medical_opinions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_requests_users_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_medical_opinions_EmployeeId",
+                table: "medical_opinions",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_requests_EmployeeId",
+                table: "requests",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_requests_OpinionId",
+                table: "requests",
+                column: "OpinionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "requests");
+
+            migrationBuilder.DropTable(
+                name: "medical_opinions");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
